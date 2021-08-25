65a55f86f8cc4d4401f5209b2b48ade7d16a733c
Aiden Tao <zwtao99@outlook.com>
2021-08-25 21:00:34 +1000

    fastjson

--- springmvc-05-json/src/main/java/com/odas/controller/UserController.java
+++ springmvc-05-json/src/main/java/com/odas/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.odas.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -93,5 +95,40 @@ public class UserController {
 
         // HH大写是24小时，hh小写是12小时
         return JsonUtils.getJson(date);
+    }
+
+    // 使用fastjson
+    @RequestMapping("/json6")
+    public String json6() {
+        List<User> userList = new ArrayList<User>();
+        User user1 = new User(1, "Odas一号", 3);
+        User user2 = new User(2, "Odas二号", 4);
+        User user3 = new User(3, "Odas三号", 5);
+
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        String jsonString = JSON.toJSONString(userList);
+
+        System.out.println("*******Java对象 转 JSON字符串*******");
+        String str1 = JSON.toJSONString(userList);
+        System.out.println("JSON.toJSONString(list)==>"+str1);
+        String str2 = JSON.toJSONString(user1);
+        System.out.println("JSON.toJSONString(user1)==>"+str2);
+
+        System.out.println("\n****** JSON字符串 转 Java对象*******");
+        User jp_user1 = JSON.parseObject(str2,User.class);
+        System.out.println("JSON.parseObject(str2,User.class)==>"+jp_user1);
+
+        System.out.println("\n****** Java对象 转 JSON对象 ******");
+        JSONObject jsonObject1 = (JSONObject) JSON.toJSON(user2);
+        System.out.println("(JSONObject) JSON.toJSON(user2)==>"+jsonObject1.getString("name"));
+
+        System.out.println("\n****** JSON对象 转 Java对象 ******");
+        User to_java_user = JSON.toJavaObject(jsonObject1, User.class);
+        System.out.println("JSON.toJavaObject(jsonObject1, User.class)==>"+to_java_user);
+
+        return jsonString;
     }
 }
