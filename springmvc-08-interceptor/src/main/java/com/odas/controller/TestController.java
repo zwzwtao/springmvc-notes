c51d57866e1076f98afa8c2f3a99783c32b2b6e7
odas <zwtao106@gmail.com>
2021-09-14 01:00:20 +1000

    interceptor

---
+++ springmvc-08-interceptor/src/main/java/com/odas/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.odas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/t1")
+    public String t1() {
+        return "ok";
+    }
+}
