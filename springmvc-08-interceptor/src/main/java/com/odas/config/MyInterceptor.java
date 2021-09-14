6eacb9584e37c1512bb9903e04dc7cf68aca81a3
odas <zwtao106@gmail.com>
2021-09-14 23:15:02 +1000

    interceptor modification

--- springmvc-08-interceptor/src/main/java/com/odas/config/MyInterceptor.java
+++ springmvc-08-interceptor/src/main/java/com/odas/config/MyInterceptor.java
@@ -10,13 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 public class MyInterceptor implements HandlerInterceptor {
 
     // 如果return true 则执行下一个拦截器，放行的意思
-    // 如果return false 则不执行下一个拦截器
+    // 如果return false 则不执行下一个拦截器，表示不放行，如果写了false程序就停住啦
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("=============== preHandle 处理前 ===============");
 
         return true;
     }
 
+    // 拦截完毕后的日志一般可以在postHandle里处理
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("=============== postHandle 处理后 ===============");
     }
