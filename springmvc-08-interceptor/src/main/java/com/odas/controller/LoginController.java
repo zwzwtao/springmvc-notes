f97f597bd86e3a60ea84b061ce6a6003108adf69
odas <zwtao106@gmail.com>
2021-09-15 03:26:07 +1000

    interceptor modification

--- springmvc-08-interceptor/src/main/java/com/odas/controller/LoginController.java
+++ springmvc-08-interceptor/src/main/java/com/odas/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.odas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -20,9 +21,22 @@ public class LoginController {
     }
 
     @RequestMapping("/login")
-    public String login(HttpSession session, String username, String password) {
+    public String login(HttpSession session, String username, Model model) {
         // 把用户的信息存在session中
         session.setAttribute("userLoginInfo", username);
+        model.addAttribute("username", username);
+
+        return "main";
+    }
+
+    @RequestMapping("/goOut")
+    public String goOut(HttpSession session) {
+        // 不要使用session.invalidate();这个是销毁session的
+        // 因为只要用户浏览器没关session一直都在的，直接销毁session服务器
+        // 就又要重新创建session，这样资源消耗会非常大
+
+        // 这里只要移除session里的userLoginInfo即可实现logout
+        session.removeAttribute("userLoginInfo");
 
         return "main";
     }
--- springmvc-08-interceptor/web/WEB-INF/jsp/main.jsp
+++ springmvc-08-interceptor/web/WEB-INF/jsp/main.jsp
@@ -7,5 +7,12 @@
 
 <h1>首页</h1>
 
+<span>当前登陆用户: ${username}</span>
+
+<p>
+    <a href="${pageContext.request.contextPath}/user/goOut">注销</a>
+
+</p>
+
 </body>
 </html>
